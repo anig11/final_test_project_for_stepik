565e9babc883ecdf54628fef48956dce244a96e0
anig11 <andrus.igor.ru>
2023-10-27 22:39:51 +0300

    Create transitions between pages

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,7 @@ import time
 import math
 import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,6 +14,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
