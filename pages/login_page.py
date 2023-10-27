24e6486253d9c4a09f3263e6489f2118cf467e76
anig11 <andrus.igor.ru>
2023-10-27 22:49:56 +0300

    Solved the error

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in browser.current_url, "The link is not correct"
+        assert "login" in self.browser.current_url, "The link is not correct"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
