2dd7f82a113319b0bf3e6d5dd26c2c252d173fce
anig11 <andrus.igor.ru>
2023-11-04 16:32:30 +0300

    Basket check implemented

---
+++ pages/basket_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators, BasePageLocators, BasketPageLocators
+from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException, TimeoutException
+import math
+
+class BasketPage(BasePage):
+    
+    def basket_should_by_empty(self):
+        assert self.is_not_element_present(*BasketPageLocators.COUNT_ITEMS),"The elements are there, but they shouldn't be"
+        
+    def should_by_empty_message(self):
+        assert self.is_not_element_present(*BasketPageLocators.COUNT_ITEMS),"The elements are there, but they shouldn't be"
+        empty_message = self.browser.find_element(*BasketPageLocators.MESSAGE_BASKET_IS_EMPTY).text     
+        assert "Your basket is empty." in empty_message,"No empty message "
+            
+            
--- test_main_page.py
+++ test_main_page.py
@@ -7,8 +7,11 @@ import math
 import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException,TimeoutException
 
-
+@pytest.mark.skip 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -16,9 +19,20 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-    
+
+@pytest.mark.skip     
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()    
+    page.should_be_login_link()
+    
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(5)
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_should_by_empty()
+    basket_page.should_by_empty_message()
